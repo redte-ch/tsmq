4e55bdff38ec5b6d397f497c663be118c22aa996
Mauko Quiroga <public@mauko.me>
2025-02-07 21:51:22 +0100

    test: add job-to-be-done test (#7)

---
+++ test/jtbd/find-a-taxi.test.ts
@@ -0,0 +1,7 @@
+import { test } from 'tap'
+
+test('find a free taxi in Paris', async (t) => {
+  // When I'm looking for an available taxi in Paris
+  // And a taxi becomes available
+  // I want to know, so I can take it and go home
+})
